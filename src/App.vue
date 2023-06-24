80e05837c9e2fe82514df6df2051caf138225629
Deivis Rodriguez <deivisjoro@yahoo.com>
2023-06-24 14:45:44 -0500

    primer commit

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<script setup>
+  import productos from './data';
+  import Ficha from "./components/Ficha.vue";
+
+  const aumentar = (index) => {
+    productos.value[index].nota++;
+    if(productos.value[index].nota>5)
+      productos.value[index].nota = 1;
+  }
+
+  const disminuir = (index) => {
+    productos.value[index].nota--;
+    if(productos.value[index].nota<1)
+      productos.value[index].nota = 5;
+
+  }
+
+</script>
+
+<template>
+  <div class="container">
+    <h1>Evalua productos</h1>
+    <div class="content">
+      <Ficha 
+        v-for="(producto, i) in productos" 
+        :key="i" 
+        :producto="producto" 
+        :index="i" 
+        @onAumentar="aumentar" 
+        @onDisminuir="disminuir" 
+      />
+    </div>
+  </div>
+  
+</template>
+
+<style scoped>
+  .container{
+    width: 60%;
+    margin-top: -50px;
+  }
+
+  h1{
+    text-align: center;
+    padding: 10px;
+    background-color: #fff;
+    margin-bottom: 10px;
+    font-size: 18px;
+    font-weight: bold;
+    border-radius: 10px;
+  }
+
+  .content{
+    border: 2px solid #666060;
+    border-radius: 15px;
+  }
+</style>
---
+++ src/components/Ficha.vue
@@ -0,0 +1,90 @@
+<script setup>
+
+    import estrella from '../assets/images/estrella.png';  
+    
+    const propiedades = defineProps({
+        producto: Object,
+        index: Number
+    })
+
+    const emisores = defineEmits(['onAumentar', 'onDisminuir']);
+
+    const aumentar = () => {
+        emisores('onAumentar', propiedades.index);
+    }
+
+    const disminuir = () => {
+        emisores('onDisminuir', propiedades.index);
+    }   
+
+</script>
+
+<template>
+  <div class="producto">
+    <div class="info">
+        {{ producto.ropa }} ({{ producto.marca }})
+    </div>
+    <div class="opciones">
+        <div class="estrellas">
+            <span v-for="i in producto.nota" :key="i">
+                <img :src="estrella" alt="imagen de estrella">
+            </span>        
+        </div>
+        <div class="botones">
+            <button @click="aumentar"> + </button>
+            <button @click="disminuir"> - </button>
+        </div>
+    </div>
+  </div>
+  
+</template>
+
+<style scoped>
+
+    .producto{
+        display: flex;
+        align-items: center;
+        padding: 10px;
+        border-bottom: 1px solid rgba(200, 200, 200, .5);
+    }
+
+    .info{
+        flex: 1;
+        background-color: #fff;
+        padding: 5px;
+        margin-right: 10px;
+    }
+
+    .opciones{
+        flex: 1;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    .estrellas img{
+        width: 20px;
+        margin: 1px;
+    }
+
+    .botones{
+        display: flex;
+    }
+
+    button{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 30px;
+        height: 30px;
+        margin: 0 5px;
+        border-radius: 50%;
+        font-size: 1.5em;
+        font-weight: bold;
+    }
+
+    button:hover{
+        cursor: pointer;
+    }
+
+</style>
